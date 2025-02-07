0b3e13c2ba320324a4f6e548db6374a2b5765093
Thoushika <130437934+thouxzzh@users.noreply.github.com>
2025-02-07 08:58:44 +0530

    Add files via upload

---
+++ JenkinsProject3.java
@@ -0,0 +1,8 @@
+public class JenkinsProject3{
+public static void main(String args[]){
+if(args.length<2){
+System.out.println("Usage:parameterInput <name><age>");
+}
+String Name=args[0];
+int Age=Integer.parseInt(args[1]);
+System.out.println("Hello "+Name+ "You are "+Age+ "old");
