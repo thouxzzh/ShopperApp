c4df635c8c479fbad0732d2f6ec54e2ba623733f
Thoushika <130437934+thouxzzh@users.noreply.github.com>
2025-02-07 09:04:06 +0530

    Update JenkinsProject3.java

--- JenkinsProject3.java
+++ JenkinsProject3.java
@@ -1,10 +1,11 @@
-public class JenkinsProject3{
-public static void main(String args[]){
-if(args.length<2){
-System.out.println("Usage:parameterInput <name><age>");
-}
-String Name=args[0];
-int Age=Integer.parseInt(args[1]);
-System.out.println("Hello "+Name+ "You are "+Age+ "old");
-}
+public class JenkinsProject3 {
+    public static void main(String args[]) {
+        if (args.length < 2) {
+            System.out.println("Usage: parameterInput <name> <age>");
+            return; 
+        }
+        String Name = args[0];
+        int Age = Integer.parseInt(args[1]);
+        System.out.println("Hello " + Name + ", You are " + Age + " years old.");
+    }
 }
